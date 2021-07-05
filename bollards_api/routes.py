32baaa8bd7ffb6e5ab5daba993970ef0db48edc5
JMA <wakxy13@gmail.com>
2021-07-05 22:00:47 +0200

    crop center of profile picture

--- bollards_api/routes.py
+++ bollards_api/routes.py
@@ -59,14 +59,38 @@ def logout():
     return redirect(url_for('home'))
 
 
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
+
 def save_picture(new_picture, folder_path):
+
+    fixed_square_size = 150
+
     random_hex = secrets.token_hex(8)
     _, file_ext = os.path.splitext(new_picture.filename)
     picture_filename = random_hex + file_ext
     picture_path = os.path.join(app.root_path, 'static', 'img', 
                                     folder_path, picture_filename)
-    output_size = (150, 150)
+    output_size = (fixed_square_size, fixed_square_size)
     i = Image.open(new_picture)
+
+    # Crop image to square
+    print(str(i.width) + " " + str(i.height))
+    i_width = i.width
+    i_height = i.height
+    if i_width > fixed_square_size and i_height > fixed_square_size:
+        i = crop_max_square(i)
+        
+    # Reduce the size of picture
     i.thumbnail(output_size, Image.ANTIALIAS)
 
     i.save(picture_path)
