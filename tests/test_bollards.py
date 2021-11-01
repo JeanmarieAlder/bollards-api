37399bd1f84edf113086b3edbefb71cfd918e0ca
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2021-11-01 03:04:35 +0100

    more bollards tests

--- tests/test_bollards.py
+++ tests/test_bollards.py
@@ -15,3 +15,18 @@ def test_manage_page(client, auth):
     resp = rv.data
     print(resp)
     assert b"<h1>Manage Bollard</h1>" in resp
+
+
+def test_modify_bollard(client, auth):
+    auth.login()
+    rv = client.post("/manage/1", data=dict(
+        b_number = 42,
+        b_letter = "z",
+        b_type = "Special",
+        images = (None, '')
+    ), follow_redirects=True)
+    resp = rv.data
+    print(resp)
+    assert b'<h1 class="m-2">Bollards List</h1>' in resp
+    assert b'<b>No 42z</b>' in resp
+    assert b'Bollard No 42 has been updated' in resp
